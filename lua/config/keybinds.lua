37512590460ab88c00fdfc3c227a9b3edf267f2c
Jay <fendler@uchicago.edu>
2026-02-27 11:12:33 -0600

    added deletion>keyboard support

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -16,6 +16,8 @@ vim.api.nvim_set_keymap("v", "k", "k", { noremap = true })
 -- copy to system clipboard
 vim.api.nvim_set_keymap("v", "<A-y>", '"+y', { noremap = true })
 vim.api.nvim_set_keymap("n", "<A-y>y", '<S-v>"+y', { noremap = true })
+vim.api.nvim_set_keymap("v", "<A-d>", '"+d', { noremap = true })
+vim.api.nvim_set_keymap("n", "<A-d>d", '<S-v>"+d', { noremap = true })
 
 -- insert blank lines
 vim.api.nvim_set_keymap("n", "<Return>", "o<Esc>", { noremap = true })
